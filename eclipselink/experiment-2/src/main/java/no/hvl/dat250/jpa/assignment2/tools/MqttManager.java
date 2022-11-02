f56288e8a45983782fe8c048a5f3be105941731a
Anthony-Leblanc <anthony.leblanc@edu.univ-paris13.fr>
2022-11-02 12:07:40 +0100

    NewClass

---
+++ eclipselink/experiment-2/src/main/java/no/hvl/dat250/jpa/assignment2/tools/MqttManager.java
@@ -0,0 +1,21 @@
+package no.hvl.dat250.jpa.assignment2.tools;
+
+/*
+
+The following class will manage the mqtt services. It will allow us to send our data to a server and
+a database. If we got enough time to develop this part it will also allow us to manage the
+authentifications to the application.
+
+The website used as sources are the following :
+  _ https://www.hivemq.com/blog/how-to-get-started-with-mqtt/
+
+The documents used as sources are the following :
+  _ hivemq-ebook-mqtt-essentials from https://mqtt.org/
+
+We will use : HiveMq as broker and as Browser Client (https://www.hivemq.com/public-mqtt-broker/)
+  user : DAT250_projectChLeRu
+  psw : projetDeMerde!666
+*/
+
+public class MqttManager {
+}
